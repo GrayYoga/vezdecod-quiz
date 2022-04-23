27cb949b620bebd2af992ffb568c6ce5515ffc81
Sergey Boiko <s.boiko@rogii.com>
2022-04-23 17:04:06 +0400

    Quiz - h2 base and liquibase migrations

---
+++ src/main/java/com/vezdecod/quiz/controller/QuizController.java
@@ -0,0 +1,26 @@
+package com.vezdecod.quiz.controller;
+
+import com.vezdecod.quiz.model.Question;
+import com.vezdecod.quiz.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Service
+public class QuizController {
+    @Autowired()
+    private QuestionRepository questionRepo;
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/question/random")
+    public Question getRandom() {
+//        return new Question("Поэт, автор поэмы Евгений Онегин", new Category("Поэты"), 200);
+        return questionRepo.getById(1);
+    }
+}
